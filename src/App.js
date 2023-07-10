717b7c3a4f46f1265d53f20c6809b827a1cd89b6
Tanvi Singh <singhvi2104@gmail.com>
2023-07-10 12:41:52 +0530

    done with first half of project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg'
 import './App.css';
+
+const pOp={
+          particles:{
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area:800
+              }
+            }
+          }
+}
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg className='particles
+      position: "absolute",zIndex: -1,top: 0,left: 0' 
+        params={pOp}
+      />
       <Navigation />
-      {/*<Logo />
+      <Logo />
+      <Rank />
       <ImageLinkForm />
-      <FAceRecognition />*/}
+      {/*<FaceRecognition />*/}
     </div>
   );
 }
