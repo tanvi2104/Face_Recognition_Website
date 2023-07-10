e684a8f402f5d5810a9e87659592716c965aa811
Tanvi Singh <117470255+tanvi2104@users.noreply.github.com>
2023-07-10 10:05:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      {/*<Logo />
+      <ImageLinkForm />
+      <FAceRecognition />*/}
+    </div>
+  );
+}
+
+export default App;
